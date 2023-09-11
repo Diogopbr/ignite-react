9cbf843758dbca1d9c84cfb98dc1995f61421f1f
Diogo Pereira <78860212+Diogopbr@users.noreply.github.com>
2023-09-11 13:33:06 -0300

    deixando igual ao curso

--- modulo-2/src/App.jsx
+++ modulo-2/src/App.jsx
@@ -1,10 +1,20 @@
-import { Header } from "./components/Header"
+import { Post } from "./Post"
+import { Header } from './components/Header'
 import './styles.css'
 
 function App() {
   return (
-   <h1>Kauan</h1>
+   <div>
+    <Header />
+
+      <Post
+        author="Diogo Pereira"
+        content="blablablablablablablablablablabla"
+      />
+      <Post
+        author="Kauan"
+        content="Diamante lixo"
+      />
+  </div>
   )
 }
-
-export default App
