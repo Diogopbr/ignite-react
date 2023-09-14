a5846c4768977983ee58c273b73c366baa8f12c9
Diogo Pereira <78860212+Diogopbr@users.noreply.github.com>
2023-09-14 12:38:12 -0300

    organização de hierarquia

--- modulo-2/src/App.jsx
+++ modulo-2/src/App.jsx
@@ -1,11 +1,11 @@
 import { Header } from './components/Header'
 import { Post } from './components/Post';
+import { Sidebar } from './components/sidebar';
 
 
 import styles from './App.module.css';
 
 import './global.css'
-import { Sidebar } from './components/sidebar';
 
 export default function App() {
 
