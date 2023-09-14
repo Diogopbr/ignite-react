5a4a0ffa24a641e8a2648d8c3decf1874303e7ce
Diogo Pereira <78860212+Diogopbr@users.noreply.github.com>
2023-09-14 13:09:40 -0300

    criação de post sem comentarios

--- modulo-2/src/components/Post.jsx
+++ modulo-2/src/components/Post.jsx
@@ -1,5 +1,30 @@
+import styles from './Post.module.css';
+
 export function Post () {
     return (
-        <h1>teste</h1>
+        <article className={styles.post}>
+            <header>
+                <div className={styles.author}>
+                    <img className={styles.avatar} src="https://github.com/diogopbr.png" />
+                    <div className={styles.authorInfo}>
+                        <strong>Diogo Pereira</strong>
+                        <span>Web Developer</span>
+                    </div>
+                </div>
+
+                <time title="14 de Setembro às 12:45h" dateTime="2023-09-14 12:45:00">Publicado há 1h</time>
+            </header>
+
+            <div className={styles.content}>
+            <p>Fala galeraa 👋</p>
+            <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+            <p><a href=''> jane.design/doctorcare</a></p>
+            <p> 
+                <a href="">#novoprojeto</a>{' '}
+                <a href="">#nlw</a>{' '}
+                <a href="">#rocketseat</a>
+            </p>
+            </div>
+        </article>
     )
 }
