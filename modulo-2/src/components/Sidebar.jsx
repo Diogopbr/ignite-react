3cbe48b0ae803d620e623d19e7e6e417acd81bae
Diogo Pereira <78860212+Diogopbr@users.noreply.github.com>
2023-10-06 14:26:46 -0300

    criação de avatar css jsx e componentes

--- modulo-2/src/components/Sidebar.jsx
+++ modulo-2/src/components/Sidebar.jsx
@@ -1,6 +1,7 @@
 import { Pen, PencilLine } from 'phosphor-react'
 
 import styles from './Sidebar.module.css';
+import { Avatar } from './Avatar';
 
 export function Sidebar () {
     return (
@@ -10,7 +11,7 @@ export function Sidebar () {
                 src="https://images.unsplash.com/photo-1682687220975-7b2df674d3ce?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=50" />
 
             <div className={styles.profile}>
-                <img className={styles.avatar} src="https://github.com/diogopbr.png" />
+                <Avatar src="https://github.com/diogopbr.png" />
 
                 <strong>Diogo Pereira</strong>
                 <span>Web Developer</span>
